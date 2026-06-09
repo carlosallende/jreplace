65f6dc68767424f7093efaeebafa2350fb92e77a
Carlos Allende <resultaquesi@gmail.com>
2026-06-09 12:40:12 +0100

    Update jreplace.py to use jreplace_core module

--- jreplace.py
+++ jreplace.py
@@ -25,28 +25,17 @@ Carlos, December 2016, upgraded to work with dir trees containing templates
 
 import sys
 import os
-import shutil
-import docx
 import openpyxl
-from docx.document import Document
-from docx.oxml.table import CT_Tbl
-from docx.oxml.text.paragraph import CT_P
-from docx.table import _Cell, Table
-from docx.text.paragraph import Paragraph
+from jreplace_core import jreplace_file, jreplace_dir
 
 
-#template='01.clausula.docx'
-#rownum=2
-#workbook='00. BBDD proyectos.xlsx'
-
-
-#basic checks
 def info():
     """
     inform users on syntax
     """
     print('syntax:  jreplace  xlsx  docx/dirtree  rownumber1 rownumber2 ...')
     return
+
 
 def prechecks():
     """
@@ -74,155 +63,43 @@ def prechecks():
     elif os.path.isdir(sys.argv[2]):
         pass
     else: 
-        print ("ERROR: template "+sys.argv[2]+" is neither a file nor a directory")
+        print("ERROR: template " + sys.argv[2] + " is neither a file nor a directory")
         info()
-        exit()        
+        exit()
 
-def iter_block_items(parent):
-    """
-    Yield each paragraph and table child within *parent*, in document order.
-    Each returned value is an instance of either Table or Paragraph. *parent*
-    would most commonly be a reference to a main Document object, but
-    also works for a _Cell object, which itself can contain paragraphs and tables.
-    """
-    if isinstance(parent, Document):
-        parent_elm = parent.element.body
-    elif isinstance(parent, _Cell):
-        parent_elm = parent._tc
-    else:
-        raise ValueError("something's not right")
-
-    for child in parent_elm.iterchildren():
-        if isinstance(child, CT_P):
-            yield Paragraph(child, parent)
-        elif isinstance(child, CT_Tbl):
-            yield Table(child, parent)
-            #pass
-
-def jreplace_file(sheet,template,rownum,tag):
-    """
-    Does the replacement job on a single file (template) for a single row 
-    in the (excel) sheet
-    """
-    #open word template
-    doc = docx.Document(template)
-    
-    #find and replace
-    #print("KEYWORDS:"),
-    for colnum in range(1,sheet.max_column+1):
-        i=0
-        keyword=str(sheet.cell(row=1, column=colnum).value)
-        target=str(sheet.cell(row=rownum, column=colnum).value)
-        if ((keyword == None) | (target == None)): continue
-        #print((keyword+'-').strip()),
-        #sys.stdout.write((keyword+'-').strip())
-        #sys.stdout.flush()
-        #print ("%s-" % keyword.strip(),end='')
-        for block in iter_block_items(doc): 
-            if hasattr(block,'text'): #deals with page breaks
-                if block.text == u'\n': continue
-            if hasattr(block,'runs'): #if it has runs it is a paragraph
-                inline = block.runs
-                #print('we have a paragraph')
-                if (len(inline) < 1): continue
-                for j in range(len(inline)):
-                    if inline[j].text == '': continue
-                    text = inline[j].text.replace(keyword,target)
-                    inline[j].text = text
-            else:                    #if it doesn't, it is a table
-                if not hasattr(block,'rows'): continue
-                #print('we have a table')
-                for row in block.rows: 
-                    if not hasattr(row,'cells'): continue
-                    for cell in row.cells:
-                        if not hasattr(cell,'paragraphs'): continue
-                        for paragraph in cell.paragraphs:
-                            #print ("paragraph text=",paragraph.text)
-                            inline=paragraph.runs
-                            for j in range(len(inline)):
-                                #print(i,j)
-                                text = inline[j].text.replace(keyword,target)
-                                inline[j].text = text
-
-        i+=1
-
-    #save output to a new doc
-    outdoc=template[0:-5]+tag+'.docx'
-    print('\n-- Writing output to '+outdoc)
-    doc.save(outdoc)
-
-def docker(arg,dirname,filenames):
-    """
-    Auxiliary function to getdocs, which picks docx files from dirs
-    """
-    #print dirname,filenames
-    for file in filenames:
-       fullname=dirname+'/'+file
-       if (os.path.isfile(fullname) & (fullname[-4:]  == 'docx')): 
-           arg.append(fullname)
-           #print ('adding '+fullname)
-
-def getdocxs(dir):
-    """
-    Gets all the docx files in a dir tree
-    """
-    files=[]
-    #os.path.walk(dir,docker,files)
-    for directory, dirnames, filenames in os.walk(dir):
-      for file in filenames:
-        fullname=dir+'/'+file
-        if (os.path.isfile(fullname) & (fullname[-4:]  == 'docx')): 
-          files.append(fullname)
-
-    return files
-
-def jreplace_dir(sheet,template,rownum,tag):
-    """
-    Does the replacement job on all docx documents in a tree dir (template) 
-    for a single row in the (excel) sheet
-    """
-    print ('template is='+template)
-    newdir=template+tag
-    shutil.copytree(template,newdir)
-    docfiles=getdocxs(newdir)
-    print (docfiles)
-    for file in docfiles:
-        jreplace_file(sheet,file,rownum,'')
-    
 
 if __name__ == "__main__":
 
-    #carry out basic checks
+    # carry out basic checks
     prechecks()
 
-    #assign parameters to variables
-    workbook=sys.argv[1]
-    template=sys.argv[2]
-    rownumbers=sys.argv[3:]
+    # assign parameters to variables
+    workbook = sys.argv[1]
+    template = sys.argv[2]
+    rownumbers = sys.argv[3:]
 
-
-    #open excel sheet
+    # open excel sheet
     wb = openpyxl.load_workbook(workbook)
-    sheetnames=wb.get_sheet_names()
-    sheet=wb.get_sheet_by_name(sheetnames[0])
+    sheetnames = wb.get_sheet_names()
+    sheet = wb.get_sheet_by_name(sheetnames[0])
 
-
-    #loop over rownumbers
+    # loop over rownumbers
     for row in rownumbers:
-        rownum=int(row)
+        rownum = int(row)
   
         if rownum > sheet.max_row:
-            print('ERROR: requested rownumber='+row+' but the excel sheet has only '+ \
-                   str(sheet.max_row)+' rows') 
+            print('ERROR: requested rownumber=' + row + ' but the excel sheet has only ' + \
+                   str(sheet.max_row) + ' rows') 
             continue
 
-        tag=("_%04d" % rownum)
+        tag = ("_%04d" % rownum)
 
         if os.path.isfile(template):
-            jreplace_file(sheet,template,rownum,tag)
+            jreplace_file(sheet, template, rownum, tag)
+            print('\n-- Writing output to ' + os.path.basename(template)[0:-5] + tag + '.docx')
         elif os.path.isdir(template):
-            jreplace_dir(sheet,template,rownum,tag)
+            jreplace_dir(sheet, template, rownum, tag)
+            print('\n-- Created output directory: ' + os.path.basename(template) + tag)
         else: 
-            print ("ERROR: template "+template+" is neither a file nor a directory")
+            print("ERROR: template " + template + " is neither a file nor a directory")
             break
-
